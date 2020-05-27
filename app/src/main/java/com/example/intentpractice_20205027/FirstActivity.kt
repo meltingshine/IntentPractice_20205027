416ed22086134f8e1d49089b41eeed5eb73d2588
meltingshine <meltingshine@gmail.com>
2020-05-27 21:48:20 +0900

    첫번째 액티비티 > 메인으로 돌아가긔  - finish() 활용

--- app/src/main/java/com/example/intentpractice_20205027/FirstActivity.kt
+++ app/src/main/java/com/example/intentpractice_20205027/FirstActivity.kt
@@ -1,12 +1,25 @@
 package com.example.intentpractice_20205027
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        goBackBtn.setOnClickListener {
+//            돌아갈때는 Intent로 돌아가지 않습니다
+//            왜냐
+//            Intent는 새로 화면을 만들어내는 효과가 있기 때문이다~
+
+//            val myIntent = Intent(this,MainActivity::class.java)
+//            startActivity(myIntent)
+            finish()
+
+        }
     }
 }
