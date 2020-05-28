dcb66ffe08328d0103e389566e034ac467e839e7
meltingshine <meltingshine@gmail.com>
2020-05-28 20:05:17 +0900

    finish하기전에 필요한 데이터 첨부. => 돌아가는 Intent는 빈 괄호로 생성 => setResult로 결과를 기록한다. => RESULT_OK로 확인을 눌렀다고 알려줌. => Intent변수를 첨부해서 데이터를 첨부.

--- app/src/main/java/com/example/intentpractice_20205027/FourthActivity.kt
+++ app/src/main/java/com/example/intentpractice_20205027/FourthActivity.kt
@@ -1,12 +1,31 @@
 package com.example.intentpractice_20205027
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_fourth.*
 
 class FourthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fourth)
+
+        goBackBtn.setOnClickListener{
+            val input = goBackMessageEdt.text.toString()
+
+            val goBackIntent = Intent()
+            goBackIntent.putExtra("message",input)
+
+            setResult(Activity.RESULT_OK,goBackIntent)
+
+            finish()
+
+
+        }
+
     }
+
+
 }
